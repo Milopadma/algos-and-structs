84268cc3a2964236d77111aa6c7b34c66aeaa2fa
mylo <milopadma@yahoo.com>
2023-01-04 16:34:03 +0800

    proper rusting this time

--- rust/data-structures/singly-linked-lists/src/main.rs
+++ rust/data-structures/singly-linked-lists/src/main.rs
@@ -1,38 +1,62 @@
 // singly linked list
 // 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7 -> 8 -> 9 -> 10 -> null
 
-struct Node {
-    data: i32,
-    next: Option<Box<Node>>,
+enum Link<T> {
+    Empty,
+    Tail {
+        data: T,
+    },
+    Link {
+        data: T,
+        next: Box<Link<T>>,
+    },
 }
 
+impl<T> Link<T> where T: Copy {
+    fn push(&mut self, x: T) {
+        match self {
+            Link::Empty => {
+                *self = Link::Tail { data: x };
+            }
+            Link::Tail { data } => {
+                *self = Link::Link {
+                    data: *data,
+                    next: Box::new(Link::Tail { data: x }),
+                };
+            }
+            Link::Link { data, next } => {
+                next.push(x);
+            }
+        }
+    }
+}
 fn main() {
     // create a new node
-    let mut head = Node {
-        data: 1,
-        next: None,
-    };
+    // let mut head = Node {
+    //     data: 1,
+    //     next: None,
+    // };
 
-    // create a new node
-    let node2 = Node {
-        data: 2,
-        next: None,
-    };
+    // // create a new node
+    // let node2 = Node {
+    //     data: 2,
+    //     next: None,
+    // };
 
-    // create a new node
-    let node3 = Node {
-        data: 3,
-        next: None,
-    };
+    // // create a new node
+    // let node3 = Node {
+    //     data: 3,
+    //     next: None,
+    // };
 
-    // link the nodes
-    head.next = Some(Box::new(node2));
-    head.next.as_mut().unwrap().next = Some(Box::new(node3));
+    // // link the nodes
+    // head.next = Some(Box::new(node2));
+    // head.next.as_mut().unwrap().next = Some(Box::new(node3));
 
-    // print the linked list
-    let mut current = &head;
-    while let Some(node) = current.next.as_ref() {
-        println!("{}", current.data);
-        current = node;
-    }
+    // // print the linked list
+    // let mut current = &head;
+    // while let Some(node) = current.next.as_ref() {
+    //     println!("{}", current.data);
+    //     current = node;
+    // }
 }
