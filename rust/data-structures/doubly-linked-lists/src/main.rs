2730eed8c4f9a0d97c20c307ba22c3c4ab21e40a
mylo <milopadma@yahoo.com>
2023-01-08 18:35:26 +0800

    added refs

--- rust/data-structures/doubly-linked-lists/src/main.rs
+++ rust/data-structures/doubly-linked-lists/src/main.rs
@@ -1,3 +1,5 @@
 fn main() {
     println!("Hello, world!");
 }
+
+// ref https://rtoch.com/posts/rust-doubly-linked-list/#:~:text=Doubly%20Linked%20List%20(or%20Linked,Stack%20%2C%20Queue%20%2C%20and%20Deque%20.
