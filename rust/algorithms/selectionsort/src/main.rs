09f34e1add4d025993df44d73a0983c824047cf2
mylo <48256865+Milopadma@users.noreply.github.com>
2023-01-10 23:52:19 +0800

    test and main

--- rust/algorithms/selectionsort/src/main.rs
+++ rust/algorithms/selectionsort/src/main.rs
@@ -5,17 +5,27 @@ use std::vec;
 // ref https://applied-math-coding.medium.com/implementing-standard-algorithms-in-rust-bubble-sort-selection-sort-5c4cfb1a78c7
 fn selection_sort<T: PartialOrd>(vector: &mut [T]){
     let (min_idx, _) = vector // get the index of the smallest element
-    .iter()
-    .enumerate()
-    .min_by(|(_, a), (_, b)| a.partial_cmp(b).unwrap())
-    .unwrap();
-    vector.swap(0, min_idx);
+    .iter() // get an iterator over the elements
+    .enumerate() // to get the index of the elements
+    .min_by(|(_, a), (_, b)| a.partial_cmp(b).unwrap()) // compare the elements
+    .unwrap(); // get the smallest element
+    vector.swap(0, min_idx); // swap the smallest element with the first element
+    // recursively call the function on the rest of the list
     if vector.len() > 1 {
         selection_sort(&mut vector[1..]);
     }
 }
 
+#[test]
+fn test_selection_sort() {
+    let mut vector = vec![5, 4, 3, 2, 1];
+    selection_sort(&mut vector);
+    assert_eq!(vector, vec![1, 2, 3, 4, 5]);
+}
+
 
 fn main() {
-
+    let mut vector = vec![5, 4, 3, 2, 1];
+    selection_sort(&mut vector);
+    println!("{:?}", vector);
 }
