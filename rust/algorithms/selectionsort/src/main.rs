fc9e5b8c2fe633c889c70dea7e3fc81a2e05cb08
mylo <48256865+Milopadma@users.noreply.github.com>
2023-01-10 23:50:21 +0800

    selection sort

--- rust/algorithms/selectionsort/src/main.rs
+++ rust/algorithms/selectionsort/src/main.rs
@@ -1,3 +1,21 @@
+use std::vec;
+
+// selection sort in rust
+// searches the list for the smallest element, then swaps it with the first element of this list, cuts off the first element and repeat
+// ref https://applied-math-coding.medium.com/implementing-standard-algorithms-in-rust-bubble-sort-selection-sort-5c4cfb1a78c7
+fn selection_sort<T: PartialOrd>(vector: &mut [T]){
+    let (min_idx, _) = vector // get the index of the smallest element
+    .iter()
+    .enumerate()
+    .min_by(|(_, a), (_, b)| a.partial_cmp(b).unwrap())
+    .unwrap();
+    vector.swap(0, min_idx);
+    if vector.len() > 1 {
+        selection_sort(&mut vector[1..]);
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+
 }
