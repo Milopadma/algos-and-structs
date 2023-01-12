bd12a2962b753195b00ce7f92ff2de7674fb5bfd
mylo <48256865+Milopadma@users.noreply.github.com>
2023-01-12 22:54:11 +0800

    init-insertionsort

---
+++ rust/algorithms/insertionsort/src/main.rs
@@ -0,0 +1,6 @@
+// ref found goldmine of data structures and algorithms in any language
+// https://github.com/TheAlgorithms/Rust/blob/master/src/sorting/README.md
+
+fn main() {
+    println!("Hello, world!");
+}
