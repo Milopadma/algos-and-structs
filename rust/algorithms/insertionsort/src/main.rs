dbc023b0b5cf79adc0dcf229283b55960e3b1902
mylo <48256865+Milopadma@users.noreply.github.com>
2023-01-13 23:56:53 +0800

    comments

--- rust/algorithms/insertionsort/src/main.rs
+++ rust/algorithms/insertionsort/src/main.rs
@@ -3,13 +3,14 @@
 
 fn insertion_sort<T: Ord + Copy>(arr: &mut [T]) {
     for i in 1..arr.len() {
-        let mut j = i; // j is the index of the element to be inserted
+        let mut j = i; // set j to i because we want to insert the element at i
         let current = arr[i]; // current is the element to be inserted
 
         while j > 0 && current < arr[j - 1] {
             // while the element to be inserted is less than the element at j - 1
+            // this is where the "move" happens
             arr[j] = arr[j - 1]; // move the element at j - 1 to j
-            j -= 1; // decrement j
+            j -= 1; // decrement j to avoid an infinite loop
         }
 
         arr[j] = current; // insert the element at j
