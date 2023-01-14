199516ac9ddd475709c695b7edc5bc6516466c23
mylo <48256865+Milopadma@users.noreply.github.com>
2023-01-14 22:27:22 +0800

    comments

--- rust/data-structures/doubly-linked-lists/src/main.rs
+++ rust/data-structures/doubly-linked-lists/src/main.rs
@@ -3,12 +3,15 @@
 
 use std::{ cell::RefCell, rc::Rc };
 
+// this struct acts as a Node in the list
+// its a struct because it has multiple fields of related properties
 struct ListNode<T> {
     item: T,
     next: Link<T>,
     prev: Link<T>,
 }
 
+// this is a type alias for the Option<Rc<RefCell<ListNode<T>>>>
 type Link<T> = Option<Rc<RefCell<ListNode<T>>>>;
 
 // ref https://rtoch.com/posts/rust-doubly-linked-list/#:~:text=Doubly%20Linked%20List%20(or%20Linked,Stack%20%2C%20Queue%20%2C%20and%20Deque%20.
