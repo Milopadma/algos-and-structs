3fa39a33371b276ff63b49c0d259b4096efb5a02
mylo <48256865+Milopadma@users.noreply.github.com>
2023-01-14 22:25:12 +0800

    implement struct and type

--- rust/data-structures/doubly-linked-lists/src/main.rs
+++ rust/data-structures/doubly-linked-lists/src/main.rs
@@ -1,5 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+// doubly linked list
+// same as singly linked list but with a prev pointer to point to the node before it
+
+use std::{ cell::RefCell, rc::Rc };
+
+struct ListNode<T> {
+    item: T,
+    next: Link<T>,
+    prev: Link<T>,
 }
+
+type Link<T> = Option<Rc<RefCell<ListNode<T>>>>;
 
 // ref https://rtoch.com/posts/rust-doubly-linked-list/#:~:text=Doubly%20Linked%20List%20(or%20Linked,Stack%20%2C%20Queue%20%2C%20and%20Deque%20.
